740b96656e13c506a57306b4043c94094da622b6
Sayali <sayalijain2003@gmail.com>
2023-11-05 13:14:39 +0530

    first commit

---
+++ src/main/java/project/sayalijain/employeemanger/EmployeemangerApplication.java
@@ -0,0 +1,13 @@
+package project.sayalijain.employeemanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemangerApplication.class, args);
+	}
+
+}
