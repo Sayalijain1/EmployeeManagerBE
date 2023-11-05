cecdaee32270563d14e62ee607f93634b6b76344
Sayali <sayalijain2003@gmail.com>
2023-11-05 13:19:03 +0530

    EmployeeManagerApplication.java file modified

--- src/main/java/project/sayalijain/employeemanger/EmployeemangerApplication.java
+++ src/main/java/project/sayalijain/employeemanger/EmployeemangerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeemangerApplication {
-
+//this is test comment
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeemangerApplication.class, args);
 	}
